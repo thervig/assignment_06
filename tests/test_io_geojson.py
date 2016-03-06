56294ce21c258d4333ffe94265971bbe9509e086
thervig <thervig@asu.edu>
2016-03-05 20:29:13 -0700

    Create test_io_geojson.py

---
+++ tests/test_io_geojson.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import unittest
+sys.path.insert(0, os.path.abspath('..'))
+
+from .. import io_geojson
+
+class TestIoGeoJson(unittest.TestCase):
+
+    def setUp(self):
+        pass
