d5d52c831db822a7bbe0fe7b4dc3a21e1cba2eed
thervig <thervig@asu.edu>
2016-03-05 21:27:35 -0700

    Update point.py

---
+++ point.py
@@ -0,0 +1,18 @@
+from .. import utils
+
+
+
+class Point(object):
+  
+  def __init__(self, x, y, mark):
+      self.x = x
+      self.y = y
+      self.mark = mark
+  
+  def check_coincident(self, test):
+      return check_coincident((self.x, self.y)), test)
+  
+  def shift_point(self, self.x, self.y):
+      point = utils.shift_point(self.x, self.y)
+      self.x = utils,getx(point)
+      self.y = utils.gety(point)
