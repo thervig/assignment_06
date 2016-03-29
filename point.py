7b1dfaf9a24d7ead400389d159229000415ab95a
thervig <thervig@asu.edu>
2016-03-29 01:01:11 -0700

    Update point.py

--- point.py
+++ point.py
@@ -13,4 +13,7 @@ class Point(object):
       return utils.check_coincident((self.x, self.y), test)
   
   def point_shift_point(self, x_shift, y_shift):
-      utils.shift_point((self.x, self.y), x_shift, y_shift)
+      test = utils.shift_point((self.x, self.y), x_shift, y_shift)
+      self.x = utils.getx(test)
+      self.y = utils.gety(test)
+      
