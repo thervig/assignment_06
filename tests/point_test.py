072f762212523b928190ef44759101399570a64e
thervig <thervig@asu.edu>
2016-03-29 00:06:23 -0700

    Update point_test.py

--- tests/point_test.py
+++ tests/point_test.py
@@ -11,22 +11,22 @@ class TestPointClass(unittest.TestCase):
     def set_attributes(self):
         x = 3
         y = 9
-        test = Point(x, y)
+        test = point.Point(x, y)
         self.assertEqual(test.x, x)
         self.assertEqual(test.y, y)
         
     def test_coincident(self):
-        Point1 = Point(0, 10)
-        Point2 = Point(0, 10)
-        Point3 = Point(0, 9)
-        Point4 = Point(1, 10)
+        Point1 = point.Point(0, 10)
+        Point2 = point.Point(0, 10)
+        Point3 = point.Point(0, 9)
+        Point4 = point.Point(1, 10)
         
         self.assertTrue(Point1.check_coincident(Point2))
         self.assertFalse(Point1.check_coincident(Point3))
         self.assertFalse(Point1.check_coinsident(Point4))
     
     def test_shift(self):
-        test = Point(0, 10)
+        test = point.Point(0, 10)
         test.shift_point(5, 5)
         
         self.assertEqual(test.x, 5)
@@ -38,7 +38,7 @@ class TestPointClass(unittest.TestCase):
         points = []
         i = 0
         for i in range(20):
-            points.append(Point(0, 0, random.choice(marks)))
+            points.append(point.Point(0, 0, random.choice(marks)))
         
         count = {}
         for i in range(len(points)):
